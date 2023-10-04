d9768ac7e69c8c79e8d0718e4f3df02cbe9f3810
renatoshi <ralcedom@gmail.com>
2023-10-04 12:15:22 -0500

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Blue: "hsl(223, 87%, 63%)",
+        PaleBlue: "hsl(223, 100%, 88%)",
+        LightRed: "hsl(354, 100%, 66%)",
+        Gray: "hsl(0, 0%, 59%)",
+        VeryDarkBlue: "hsl(209, 33%, 12%)",
+        CustomBlue: "hsl(223, 100%, 77%)",
+
+      },
+      fontFamily: {
+        LibeFranklin: ['Libre Franklin', "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+};
